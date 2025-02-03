e97933f4a824b6eb288438a125bab42456f30236
Akshay Patil <studyakshaystudy@outlook.com>
2025-02-04 01:58:20 +0800

    first commit

---
+++ src/main/java/com/e_todos/apidevelopment3party_todos/ApiDevelopment3partyTodosApplication.java
@@ -0,0 +1,13 @@
+package com.e_todos.apidevelopment3party_todos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDevelopment3partyTodosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiDevelopment3partyTodosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/e_todos/apidevelopment3party_todos/ApiDevelopment3partyTodosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_todos.apidevelopment3party_todos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDevelopment3partyTodosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
